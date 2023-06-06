a8ba82d748eccc8f26d44110987e376a929f5c71
SanguchoMela <melany01-03@hotmail.com>
2023-06-06 07:32:12 -0500

    en Figuras2D ingreso de atributo nLados con su respectivo constructor, setter y getter

--- src/Figuras2D.java
+++ src/Figuras2D.java
@@ -1,2 +1,15 @@
 public class Figuras2D {
+    int nLados; //numero de lados
+    public Figuras2D(int nLados) {
+        this.nLados = nLados;
+    }
+    public int getnLados() {
+        return nLados;
+    }
+
+    public void setnLados(int nLados) {
+        this.nLados = nLados;
+    }
+
+    areaCuadrado(){}
 }
